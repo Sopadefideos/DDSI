d49651144edddd81ba6f6abc63b7eecb8c1b257d
Antonio Marfil Sánchez <antonio10marsan@gmail.com>
2020-10-28 20:01:03 +0100

    V1

--- DDSI/settings.py
+++ DDSI/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'DDSI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/antoniomarfil/Documents/DDSI/DDSI/templates'],
+        'DIRS': ['/Users/antoniomarfil/Documents/GitHub/DDSI/MusicApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
