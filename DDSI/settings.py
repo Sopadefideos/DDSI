8338f8c38a7ee0ca334a97ca53719efc70649824
Antonio Marfil Sánchez <antonio10marsan@gmail.com>
2020-10-28 20:02:16 +0100

    V1

--- DDSI/settings.py
+++ DDSI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MusicApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'DDSI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/antoniomarfil/Documents/DDSI/DDSI/templates'],
+        'DIRS': ['/Users/antoniomarfil/Documents/GitHub/DDSI/MusicApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
