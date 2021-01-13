1bbbbd61a976c44e557b70d476db87278fac2a66
Antonio Marfil Sánchez <antonio10marsan@gmail.com>
2021-01-13 06:47:21 +0100

    create admin

--- BeatShark/BeatSharkApp/admin.py
+++ BeatShark/BeatSharkApp/admin.py
@@ -4,4 +4,11 @@ from .models import *
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Genero)
+admin.site.register(Artista)
+admin.site.register(Album)
 admin.site.register(Cancion)
+admin.site.register(FavAlbum)
+admin.site.register(FavCancion)
+admin.site.register(FavArtista)
+admin.site.register(AlbumCancion)
+admin.site.register(CancionArtista)
